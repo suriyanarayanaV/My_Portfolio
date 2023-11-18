65fe387f99606611c1f1231e34e47348c43db178
Suriyanarayana Vaithiyanathan <suriyanarayana1999@gmail.com>
2023-11-18 08:27:21 +0530

    added video component and shealth360

--- src/components/videoComponent/VideoPlayer.js
+++ src/components/videoComponent/VideoPlayer.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./VideoPlayer.css";
 const VideoPlayer = (props) => {
+  const [load, setlad] = useState(false);
   return (
     <div className="videoModal">
       <div className="cameraHole" />
-      <video controls className="videoPlayerModal">
+      <video width={"90%"} controls className="videoPlayerModal">
         <source src={props.src} type="video/mp4" />
       </video>
       <button className="videoPlayerCloseBtn" onClick={props.hide}>
